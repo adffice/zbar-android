d74416a8227c1dbd2dcd4c8fec251303bbee32bc
Dallas.Cao <adffice@gmail.com>
2014-12-22 16:43:46 +0800

    更新README

---
+++ README.md
@@ -0,0 +1,8 @@
+#zbar-android
+
+----
+## 使用方法：
+
+该工程基于android-studio 1.0开发。
+
+output/ 目录下是已经编译好的库文件，如要使用请将文件copy至你工程下libs/目录下。
--- app/src/main/java/com/gmail/adffice/zbar/sample/CaptureActivity.java
+++ app/src/main/java/com/gmail/adffice/zbar/sample/CaptureActivity.java
@@ -422,14 +422,17 @@ public class CaptureActivity extends Activity implements
     }
 
     private int getStatusBarHeight() {
-        try {
-            Class<?> c = Class.forName("com.android.internal.R$dimen");
-            Object obj = c.newInstance();
-            Field field = c.getField("status_bar_height");
-            int x = Integer.parseInt(field.get(obj).toString());
-            return getResources().getDimensionPixelSize(x);
-        } catch (Exception e) {
-            e.printStackTrace();
+        int v = this.getWindow().getAttributes().flags & WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        if (v != WindowManager.LayoutParams.FLAG_FULLSCREEN) {
+            try {
+                Class<?> c = Class.forName("com.android.internal.R$dimen");
+                Object obj = c.newInstance();
+                Field field = c.getField("status_bar_height");
+                int x = Integer.parseInt(field.get(obj).toString());
+                return getResources().getDimensionPixelSize(x);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         return 0;
     }
